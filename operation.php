be8441183c177a83e58433c66e54fe05a62e7985
Ivan <volkivanv@mail.ru>
2024-09-27 23:12:21 +0300

    first commit

---
+++ operation.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 4;
+$b = 5;
+
+
+function add($arg1, $arg2) : float {
+    return $arg1 + $arg2;
+}
+
+function sub($arg1, $arg2) : float {
+    return $arg1 - $arg2;
+}
+
+function multiplicate($arg1, $arg2) : float {
+    return $arg1 * $arg2;
+}
+
+function div($arg1, $arg2) : float {
+    return $arg1 / $arg2;
+}
+
+echo "summ is: ".add($a,$b)."\n";
+echo "subtraction is: ".sub($a,$b)."\n";
+echo "product is: ".multiplicate($a,$b)."\n";
+echo "quotient is: ".div($a,$b)."\n";
+
+//docker run --rm -v ${pwd}:/cli php:8.2-cli php /cli/operation.php
